381b9b10eb1a5f42cb2cfc4d7ffde4987af592b1
Lillith Rose <imlvnaa@gmail.com>
2025-04-30 13:35:10 -0400

    server listing

---
+++ src/routes/(pages)/listing/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const subdomains = {
+		zap: 'OpenShock Instance',
+		localfiles: 'My Local Music Index',
+		shig: 'Kemomimichan API :3',
+		img: 'Immich (Invite Only)',
+		vault: 'Vaultwarden Instance (Invite Only)'
+	};
+</script>
+
+<div class="size-full *:text-xl *:p-3 underline flex flex-col items-center justify-center">
+	<a href="/">This Website</a>
+	{#each Object.entries(subdomains) as [sub, title]}
+		<a href="https://{sub}.lilyy.gay">{title}</a>
+	{/each}
+</div>
