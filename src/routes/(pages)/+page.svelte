b197f20fe6f59ed0b21209292719830fae07a3be
Lillith <imlvnaa@gmail.com>
2025-08-24 17:20:09 -0400

    Update +page.svelte

--- src/routes/(pages)/+page.svelte
+++ src/routes/(pages)/+page.svelte
@@ -97,7 +97,7 @@
 		<li use:tooltip={'This is not a joke.'}>My pronouns are Pup/Pups</li>
 		<li>Trans</li>
 		<li>Lesbian-Leaning</li>
-		<li use:tooltip={'i love you sammy, alexis, ashter, and rush~<3'}>Taken</li>
+		<li use:tooltip={'i love you moxx and alexis~<3'}>Taken</li>
 	</ul>
 	<h2 class="text-3xl">I Know:</h2>
 	<ul class="list-disc list-outside">
