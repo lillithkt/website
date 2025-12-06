ddb4a8cc0d7f0680f4ad3e7f8198dccb13fde4b6
Lillith Rose <lillithrosepup@gmail.com>
2025-12-06 17:30:42 -0500

    test

--- src/routes/vrchat/+page.server.ts
+++ src/routes/vrchat/+page.server.ts
@@ -3,6 +3,7 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
   const currentUser = await vrc.getCurrentUser();
+  console.log(currentUser);
   return {
     currentUser: currentUser.data
   }
