38cd7b583d04e865eac4ec99eebd1172bf131233
Lillith Rose <lillithrosepup@gmail.com>
2025-12-07 01:51:52 -0500

    fix env when building

--- src/lib/server/environment.ts
+++ src/lib/server/environment.ts
@@ -3,8 +3,17 @@ import { z } from "zod";
 
 const envSchema = z.object({
   AUTHPROXY_URL: z.url().optional(),
-  AUTHPROXY_KEY: z.string().length(36),
+  AUTHPROXY_KEY: z.string().length(36).optional(),
+}).partial()
+.refine(
+  v => !v.AUTHPROXY_URL || v.AUTHPROXY_KEY,
+  { message: "AUTHPROXY_KEY required when AUTHPROXY_URL is set" }
+)
+.transform(obj => {
+  return Object.values(obj).every(v => v === undefined) ? {} : obj
 })
 
 
-export default process.env.BUILDING ? {} as z.infer<typeof envSchema> : envSchema.parse(env);
+
+const environment = envSchema.parse(env);
+export default environment;
